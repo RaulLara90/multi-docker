7c3c376b212fcd6199b838e21f5cafaedd8b78fc
RLA04 <RLA04@ServiceDesks-MacBook-Pro.local>
2019-11-21 15:09:36 +0100

    git

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -35,9 +35,9 @@ class Fib extends Component {
   };
 
   renderSeenIndexes() {
-    return this.state.seenIndexes
-            .map(({ number }) => number).join(', ');
+    return this.state.seenIndexes.map(({ number }) => number).join(', ');
   }
+
 
   renderValues() {
     const entries = [];
